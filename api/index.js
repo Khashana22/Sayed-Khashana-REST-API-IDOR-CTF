8fb745ee85be365e78ac77065ad91af78911236a
Sayed Khashana <Khashana22@users.noreply.github.com>
2026-09-11 00:40:12 +0300

    fix: add Vercel serverless adapter and specify Node 22 engine

---
+++ api/index.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+// Set writable data directory for serverless environments (AWS Lambda / Vercel)
+process.env.DATA_DIR = process.env.DATA_DIR || "/tmp/data";
+
+const { server } = require("../server.js");
+
+module.exports = (req, res) => {
+  server.emit("request", req, res);
+};
